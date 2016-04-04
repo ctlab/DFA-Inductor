c7f5d4867fac930b204a4ce6ea5fbe392c5d47f2
Ilya Zakirzyanov <ilya.zakirzyanov@gmail.com>
2016-04-04 18:19:45 +0300

    Buffer optimizations are implemented

--- src/misc/Buffer.java
+++ src/misc/Buffer.java
@@ -1,17 +1,27 @@
 package misc;
 
 import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Buffer {
 	private PrintWriter pw;
 	private int countClauses;
+	private Set<Integer> trueVars;
+	private Set<Integer> falseVars;
 
 	public Buffer(PrintWriter pw) {
 		this.pw = pw;
 		this.countClauses = 0;
+		this.trueVars = new HashSet<>();
+		this.falseVars = new HashSet<>();
 	}
 
 	public void addClause(String s) {
+		s = simplify(s);
+		if (s.isEmpty() || isConstant(s)) {
+			return;
+		}
 		if (!s.endsWith(" ")) {
 			s += " ";
 		}
@@ -21,6 +31,10 @@ public class Buffer {
 	}
 
 	public void addClause(StringBuilder s) {
+		s = new StringBuilder(simplify(s));
+		if (s.length() == 0 || isConstant(s)) {
+			return;
+		}
 		if (!s.substring(s.length() - 2).equals(" ")) {
 			s.append(" ");
 		}
@@ -34,6 +48,10 @@ public class Buffer {
 		for (int literal : literals) {
 			sb.append(literal).append(" ");
 		}
+		sb = new StringBuilder(simplify(sb));
+		if (sb.length() == 0 || isConstant(sb)) {
+			return;
+		}
 		sb.append("0");
 		pw.println(sb);
 		countClauses++;
@@ -41,5 +59,50 @@ public class Buffer {
 
 	public int nClauses() {
 		return countClauses;
+	}
+
+
+	private <T extends CharSequence> String simplify(T s) {
+		int i;
+		String ans = "";
+		for (String var: s.toString().split(" ")) {
+			i = Integer.valueOf(var);
+			if (trueVars.contains(i)) {
+				if (i > 0) {
+					return "";
+				} else {
+					continue;
+				}
+			}
+			if (falseVars.contains(i)) {
+				if (i < 0) {
+					return "";
+				} else {
+					continue;
+				}
+			}
+			ans += var + " ";
+		}
+		return ans;
+	}
+
+	private <T extends CharSequence> boolean isConstant(T s) {
+		String str = s.toString();
+		if (str.split(" ").length == 1) {
+			int c = Integer.valueOf(str.trim());
+			if (c > 0) {
+				if (!trueVars.contains(c)) {
+					trueVars.add(c);
+					return false;
+				}
+			} else {
+				if (!falseVars.contains(c)) {
+					falseVars.add(c);
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
 	}
 }
